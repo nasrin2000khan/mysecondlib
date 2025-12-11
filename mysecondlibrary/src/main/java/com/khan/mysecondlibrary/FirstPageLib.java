e839ff5d2108b7a405fa14faff490a107d80afb5
Nasrin Khan <nasrin2000khan2000@gmail.com>
2025-12-11 14:30:29 +0430

    Add FirstPageLib and SecondPageLib

---
+++ mysecondlibrary/src/main/java/com/khan/mysecondlibrary/FirstPageLib.java
@@ -0,0 +1,12 @@
+package com.khan.mysecondlibrary;
+
+public class FirstPageLib {
+
+    public  int myAddition(int a, int b){
+        return a+b;
+    }
+
+    public  int mySubstraction(int a, int b){
+        return a-b;
+    }
+}
---
+++ mysecondlibrary/src/main/java/com/khan/mysecondlibrary/SecondPageLib.java
@@ -0,0 +1,17 @@
+package com.khan.mysecondlibrary;
+
+public class SecondPageLib {
+    public SecondPageLib(){
+
+    }
+
+
+    public  int myMultiplication(int a, int b){
+        return a*b;
+    }
+
+    public  int myDivision(int a, int b){
+        return a/b;
+    }
+
+}
